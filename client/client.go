e0534f03726de554207f7447b1d3d036722e6be8
Dan Potepa <dan@danpotepa.co.uk>
2020-07-01 22:29:13 +0100

    add username label

--- client/client.go
+++ client/client.go
@@ -3,26 +3,25 @@ package client
 import (
 	"dev.to-prom-exporter/models"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
+)
+
+const (
+	baseUrl = "https://dev.to/api"
 )
 
 type DevtoClient struct {
 	ApiKey string
 }
 
-func (c DevtoClient) GetUserArticleData() ([]models.Article, error) {
+func (c *DevtoClient) GetUserArticleData() ([]models.Article, error) {
 
 	articles := []models.Article{}
 
-	r, err := http.NewRequest("GET", "https://dev.to/api/articles/me", nil)
-	if err != nil {
-		return articles, err
-	}
-	r.Header.Set("Content-Type", "application/json")
-	r.Header.Set("Api-Key", c.ApiKey)
-
-	resp, err := http.DefaultClient.Do(r)
+	resp, err := c.makeApiCall("/articles/me")
 	if err != nil {
 		return articles, err
 	}
@@ -38,4 +37,47 @@ func (c DevtoClient) GetUserArticleData() ([]models.Article, error) {
 	}
 
 	return articles, nil
+}
+
+func (c *DevtoClient) GetUserDetails()(models.User, error){
+	user := models.User{}
+
+	resp, err := c.makeApiCall("/users/me")
+	if err != nil {
+		return user, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return user, err
+	}
+	defer resp.Body.Close()
+
+	if err := json.Unmarshal(body, &user); err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (c *DevtoClient) makeApiCall(endpoint string) (*http.Response, error){
+
+	endpoint = strings.TrimPrefix(endpoint, "/")
+
+	url := fmt.Sprintf("%s/%s", baseUrl, endpoint)
+
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header.Set("Content-Type", "application/json")
+	r.Header.Set("Api-Key", c.ApiKey)
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
--- main.go
+++ main.go
@@ -68,29 +68,29 @@ func probeHandler(w http.ResponseWriter, r *http.Request, client *client.DevtoCl
 func Probe(registry *prometheus.Registry, client *client.DevtoClient) (success bool) {
 
 	var (
-		totalArticlesGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		totalArticlesGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: "devto",
 			Name:      "published_articles_total",
 			Help:      "Displays the total number of users published articles",
-		})
+		}, []string{"user"})
 
 		totalViewsGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: "devto",
 			Name:      "article_views_total",
 			Help:      "Displays total number of views of users articles",
-		}, []string{"article_id"})
+		}, []string{"article_id", "user"})
 
 		totalReactionGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: "devto",
 			Name:      "article_reactions_total",
 			Help:      "Displays total number of reactions to users articles",
-		}, []string{"article_id"})
+		}, []string{"article_id", "user"})
 
 		totalCommentsGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: "devto",
 			Name:      "article_comments_total",
 			Help:      "Displays total number of comments to users articles",
-		}, []string{"article_id"})
+		}, []string{"article_id", "user"})
 
 		probeDuration = prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "devto",
@@ -111,6 +111,13 @@ func Probe(registry *prometheus.Registry, client *client.DevtoClient) (success b
 		log.Println("unable to get data:", err.Error())
 		return false
 	}
+	user, err := client.GetUserDetails()
+	if err != nil {
+		log.Println("unable to get data:", err.Error())
+		return false
+	}
+	username := user.Username
+
 	duration := time.Since(start).Seconds()
 	probeDuration.Set(duration)
 
@@ -119,16 +126,16 @@ func Probe(registry *prometheus.Registry, client *client.DevtoClient) (success b
 	for _, a := range articles {
 		id := strconv.Itoa(a.ID)
 
-		totalViewsGauge.WithLabelValues(id).Set(float64(a.PageViewsCount))
-		totalReactionGauge.WithLabelValues(id).Set(float64(a.PublicReactionsCount))
-		totalCommentsGauge.WithLabelValues(id).Set(float64(a.CommentsCount))
+		totalViewsGauge.WithLabelValues(id,username).Set(float64(a.PageViewsCount))
+		totalReactionGauge.WithLabelValues(id,username).Set(float64(a.PublicReactionsCount))
+		totalCommentsGauge.WithLabelValues(id, username).Set(float64(a.CommentsCount))
 
 		if a.Published {
 			totalPublishedArticles++
 		}
 	}
 
-	totalArticlesGauge.Set(float64(totalPublishedArticles))
+	totalArticlesGauge.WithLabelValues(username).Set(float64(totalPublishedArticles))
 
 	return true
 }
